20f518061f2a51d6f3a502cfb74c0414136fa74b
gims-inc <gabrielmusanga@gmail.com>
2023-04-19 14:33:36 +0300

    basic dict --

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,9 +16,8 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item in the cache
         """
-        if key is None or item is None:
-            pass
-        self.cache_data.update({key: item})
+        if key and item:
+            self.cache_data.update({key: item})
 
     def get(self, key):
         """Get an item by key
