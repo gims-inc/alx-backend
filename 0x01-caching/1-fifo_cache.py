e50cece1770c5a9106df613626846f227ee1ac8c
gims-inc <gabrielmusanga@gmail.com>
2023-04-19 19:12:49 +0300

    fifo caching --d

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" FIFO """
+""" FIFO  caching implimentation"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
     """FIFO cache class
+
+    Args:
+        BaseCaching (class): Basic class for this class
     """
 
     def __init__(self):
-        """Initialise
-        """
         super().__init__()
 
     def put(self, key, item):
         """Add an item in the cache
+
+        Args:
+            key ([type]): key of dictionary
+            item ([type]): item to insert in dictionary
         """
         if key and item:
             self.cache_data.update({key: item})
@@ -24,7 +29,10 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """Get an item by key
+
+        Args:
+            key ([type]): key to search into cache_data
         """
-        if key or key in self.cache_data:
+        if key in self.cache_data:
             return self.cache_data[key]
         return None
