3a9fb419e980f6b749b5bed4289eb07701ca8b2d
gims-inc <gabrielmusanga@gmail.com>
2023-04-19 15:45:27 +0300

    FIFO_cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" FIFO """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO cache class
+    """
+
+    def __init__(self):
+        """Initialise
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache
+        """
+        if key and item:
+            self.cache_data.update({key: item})
+            first_key_in = list(self.cache_data.keys())[0]
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.cache_data.pop(first_key_in)
+                print('DISCARD: {}'.format(first_key_in))
+
+    def get(self, key):
+        """Get an item by key
+        """
+        if key not in self.cache_data:
+            return None
+        return self.cache_data[key]
