e51e13931cfe947aec37aa8c12baac0c8fa99e0d
gims-inc <gabrielmusanga@gmail.com>
2023-05-12 13:00:01 +0300

    --lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ LRU  caching implimentation"""
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
@@ -12,6 +13,7 @@ class LRUCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Add an item in the cache
@@ -20,13 +22,16 @@ class LRUCache(BaseCaching):
             key ([type]): key of dictionary
             item ([type]): item to insert in dictionary
         """
-        if key and item:
-            self.cache_data.update({key: item})
-            lru_key = list(self.cache_data.keys())[0]
-
-        if len(self.cache_data) > self.MAX_ITEMS:
-            self.cache_data.pop(lru_key)
-            print('DISCARD: {}'.format(lru_key))
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key
@@ -34,6 +39,6 @@ class LRUCache(BaseCaching):
         Args:
             key ([type]): key to search into cache_data
         """
-        if key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
