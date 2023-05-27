5b590c75571d185a04c6c8d10b40ef24ca595b3b
gims-inc <gabrielmusanga@gmail.com>
2023-05-27 18:49:57 +0300

    kue -creator -processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,17 +1,26 @@
 const kue = require('kue');
+
 const queue = kue.createQueue();
+
 const data = {
     phoneNumber: '99999',
     message: 'MESSAGE',
 }
+// queue.create('push_notification_code', data).save((err, job) => {
+//     if (err) return console.log(err);
+//     console.log(`Notification job created: ${job.id}`);
+//     });
+    
 const job = queue.create('push_notification_code', data).save((err) => {
     if(!err) console.log(`Notification job created: ${job.id}`);
 });
 
-job.on('complete', () => {
-    console.log('Notification job completed');
-});
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
 
-job.on('failed', () => {
-    console.log('Notification job failed');
-});
+// job.on('progress', (progress) => console.log('Notification job progress:', progress));
+// job.on('error', (err) => console.log('Notification job error:', err));
+// job.on('paused', () => console.log('Notification job paused'));
+// job.on('active', () => console.log('Notification job active'));
+// job.on('timeout', () => console.log('Notification job timed out'));
+// job.on('progress', (progress) => console.log('Notification job progress:', progress));
--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,9 +1,16 @@
 const kue = require('kue');
 const queue = kue.createQueue();
+
 const sendNotification = (phoneNumber, message) => {
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 };
 
+// queue.process('send notification', (job, done) => {
+//     const phoneNumber = job.data.phoneNumber;
+//     const message = job.data.message;
+//     sendNotification(phoneNumber, message);
+//     done();
+//     });
 
 queue.process('push_notification_code', (job, done) => {
     sendNotification(job.data.phoneNumber, job.data.message);
