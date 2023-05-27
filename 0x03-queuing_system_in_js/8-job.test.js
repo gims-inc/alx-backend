7eee7d8bba84ac2f07ae2f940dd76381ae4523e6
gims-inc <gabrielmusanga@gmail.com>
2023-05-27 21:03:02 +0300

    test

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,41 +1,59 @@
-import createPushNotificationsJobs from './8-job.js';
 import { expect } from 'chai';
+import createPushNotificationsJobs from './8-job.js';
+
 const kue = require('kue');
 
 const queue = kue.createQueue();
+
 const listJobs = [
-  { phoneNumber: '0002', message: 'Message_2'},
-  { phoneNumber: '0003', message: 'Message_3'},
-  { phoneNumber: '0004', message: 'Message_4'},
-  { phoneNumber: '0005', message: 'Message_5'},
-  { phoneNumber: '0006', message: 'Message_6'},
+  {
+    phoneNumber: '0002',
+    message: 'Message_2',
+  },
+  {
+    phoneNumber: '0003',
+    message: 'Message_3',
+  },
+  {
+    phoneNumber: '0004',
+    message: 'Message_4',
+  },
+  {
+    phoneNumber: '0005',
+    message: 'Message_5',
+  },
 ];
 
-
-before(() => {
-  queue.testMode.enter();
-});
-
-afterEach(() => {
-  queue.testMode.clear();
-});
-
 after(() => {
-  queue.testMode.exit()
+  queue.testMode.exit();
 });
 
 describe('createPushNotificationsJobs', () => {
-  it('Valid Data', () => {
-    createPushNotificationsJobs(listJobs, queue);
-    expect(queue.testMode.jobs.length).to.equal(6);
-    createPushNotificationsJobs(listJobs, queue);
-    expect(queue.testMode.jobs.length).to.equal(12);
+  beforeEach(() => {
+    queue.testMode.enter();
   });
 
-  it('Invalid Data', () => {
+  // it('should return a push notification job', () => {
+  //   const notificationObj = createPushNotificationsJobs(listJobs, queue);
+  //   expect(notificationObj).to.be.an('object');
+  // });
+
+  it('valid Data', () => {
+    createPushNotificationsJobs(listJobs, queue);
+    expect(queue.testMode.jobs.length).to.equal(4);
+
+    createPushNotificationsJobs(listJobs, queue);
+    expect(queue.testMode.jobs.length).to.equal(8);
+  });
+
+  it('invalid Data', () => {
     expect(() => createPushNotificationsJobs('', queue)).to.throw(Error);
     expect(() => createPushNotificationsJobs(123, queue)).to.throw(Error);
-    expect(() => createPushNotificationsJobs({id: 120}, queue)).to.throw(Error);
+    expect(() => createPushNotificationsJobs({ id: 120 }, queue)).to.throw(Error);
     expect(() => createPushNotificationsJobs(NaN, queue)).to.throw(Error);
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
   });
 });
