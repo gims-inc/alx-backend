abd2a831fab95b085b2d29a27d9e3dac7c0a1bad
gims-inc <gabrielmusanga@gmail.com>
2023-05-27 17:52:20 +0300

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,13 +1,9 @@
 const redis = require("redis");
 const client = redis.createClient();
 
-client.on("error", (error) => {
-    console.log(`Redis client connected to the server: ${error}`);
-});
+client.on("error", (error) => console.log(`Redis client connected to the server: ${error}`));
 
-client.on("ready", () => {
-    console.log("Redis client connected to the server");
-});
+client.on("ready", () => console.log("Redis client connected to the server"));
 
 const publishMessage = (message, time) => {
     setTimeout(() => {
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,23 +1,20 @@
-const redis = require("redis");
+const redis = require('redis');
+
 const client = redis.createClient();
 
-client.on("error", (error) => {
-    console.log(`Redis client connected to the server: ${error}`);
-});
+client.on('error', (error) => console.log(`Redis client connected to the server: ${error}`));
 
-client.on("ready", () => {
-    console.log("Redis client connected to the server");
-});
+client.on('ready', () => console.log('Redis client connected to the server'));
 
-client.on("message", (channel, message) => {
-    if (channel.localeCompare("holberton school channel") === 0) {
-	console.log(message);
+client.on('message', (channel, message) => {
+  if (channel.localeCompare('holberton school channel') === 0) {
+    console.log(message);
 
-	if (message.localeCompare("KILL_SERVER") === 0) {
+    if (message.localeCompare('KILL_SERVER') === 0) {
 	    client.unsubscribe(channel);
 	    client.quit();
-	}
     }
+  }
 });
 
-client.subscribe("holberton school channel");
+client.subscribe('holberton school channel');
