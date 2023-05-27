7eba2d8a569ddc53f9818c12126762d5c73873ff
gims-inc <gabrielmusanga@gmail.com>
2023-05-27 19:12:20 +0300

    Job processor x Track progress and errors with Kue: Create the Job creator

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,25 +1,26 @@
 const kue = require('kue');
+
 const queue = kue.createQueue();
 
 const blacklistedPhone = [
-    '4153518780',
-  '4153518781'
+  '4153518780',
+  '4153518781',
 ];
 
 const sendNotification = (phoneNumber, message, job, done) => {
-    job.progress(0, 100);
+  job.progress(0, 100);
 
-    if (blacklistedPhone.includes(phoneNumber)) {
-	done(Error(`Phone number ${phoneNumber} is blacklisted`));
-    } else {
-	job.progress(50, 100);
-	console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-	done();
-    }
+  if (blacklistedPhone.includes(phoneNumber)) {
+    done(Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
+  }
 };
 
 queue.process('push_notification_code_2', 2, (job, done) => {
-    const { phoneNumber, message } = job.data;
+  const { phoneNumber, message } = job.data;
 
-    sendNotification(phoneNumber, message, job, done);
+  sendNotification(phoneNumber, message, job, done);
 });
